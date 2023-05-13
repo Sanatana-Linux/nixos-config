017a602162006728472c24ebf1cd5b5d2f1271d8
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2023-05-12 21:11:32 -0700

    changeup

--- flake.nix
+++ flake.nix
@@ -88,8 +88,6 @@
 
       nixpkgs-f2k.overlays.default
         (final: prev: let inherit (final) system; in {
-					unstable = import inputs.unstable { inherit config system; };
-					cosmic-comp = inputs.cosmic.packages.${system}.default;
 					awesome-git-luajit = prev.awesome-git.override {
 						lua = prev.luajit;
 					};

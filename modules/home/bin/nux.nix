9e767de1ab5dd1b404db3e7aedf5d226c9177073
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2023-05-19 12:54:32 -0700

    Nano appearing over nvim still, working that out

--- modules/home/bin/nux.nix
+++ modules/home/bin/nux.nix
@@ -3,80 +3,91 @@
 with pkgs;
 
 writeScriptBin "nux" ''
-  #!/usr/bin/env bash
-  dots="/etc/nixos"
+ #!/usr/bin/env bash
+ dots="/etc/nixos"
 
 
 
-  function help() {
-      cat <<EOF
-  Usage: nux [OPTION] [OPTION]
+function help() {
+    cat <<EOF
+Usage: nas [OPTION] [OPTION]
 
-  Options:
-      help         show this text
-      +------------------------------------------------------------+
-      sync         pull config from git repo, then commit and push
-      rebuild      rebuild configuration for host
-      vm           build a vm
-      rollback     rollback to previous generation
-      update       update flake
-      clean        clean and garabge collect store
-  EOF
-  }
+Options:
+    help         show this text
 
-  function sync() {
-      echo "Syncing nix config"
-      cd $dots \
-      &&  git pull \
-      &&  git add . \
-      &&  git commit \
-      &&  git push \
-      || echo "Error with location of Nix Configuration" \
-      && exit
-  }
+    clean        clean and garabge collect store
+    full         sync and rebuild configuration
+    rebuild      rebuild configuration for host
+    rollback     rollback to previous generation
+    search       search packages available
+    sync         pull config from git repo, then commit and push
+    update       update flake
+    vm           build a vm
+EOF
+}
 
-  function rebuild() {
-      echo "Rebuilding config"
-      doas nixos-rebuild --flake /etc/nixos#"$2" --impure switch
-  }
+function sync() {
+    echo "Syncing nix config"
+    cd $dots \
+    &&  git pull \
+    &&  git add . \
+    &&  git commit \
+    &&  git push \
+    || echo "Error with location of Nix Configuration" \
+    && exit
+}
 
-  function vm() {
-      echo "Creating VM"
-      doas nixos-rebuild --flake /etc/nixos#"$2" --impure build-vm
-  }
+function rebuild() {
+    echo "Rebuilding config"
+    doas nixos-rebuild --flake $dots#"$2" --impure switch
+}
 
-  function rollback() {
-      echo "Rolling back"
-      doas nixos-rebuild --rollback switch
-  }
+function vm() {
+    echo "Creating VM"
+    doas nixos-rebuild --flake $dots#"$2" --impure build-vm
+}
 
-  function update() {
-      echo "Updating flake"
-      doas nix flake update $dots
-  }
+function full() {
+    nix-sync
+    nix-rebuild
+}
 
-  function clean() {
-      echo "Clearing store"
-      doas nix-store --gc
-      echo "Removing old generations"
-      doas nix-env --delete-generations old
-      echo "Collecting system garbage"
-      doas nix-collect-garbage -d
-      echo "Collecting system garbage"
-      nix-collect-garbage -d
-  }
+function rollback() {
+    echo "Rolling back"
+    doas nixos-rebuild --rollback switch
+}
 
+function update() {
+    echo "Updating flake"
+    doas nix flake update $dots
+}
 
+function clean() {
+    echo "Clearing store"
+    doas nix-store --gc
+    echo "Removing old generations"
+    doas nix-env --delete-generations old
+    echo "Collecting system garbage"
+    doas nix-collect-garbage -d
+    echo "Collecting system garbage"
+    nix-collect-garbage -d
+}
 
-  case "$1" in
-      sync)      sync ;;
-      rebuild)   rebuild "$@";;
-      vm)        vm "$@";;
-      rollback)  rollback ;;
-      update)    update ;;
-      clean)     clean ;;
-      help)    help ;;
-      *)       help ;;
-  esac
+function search() {
+   nps -C=description --separator=true "$2"
+}
+
+case "$1" in
+    sync)      sync ;;
+    rebuild)   rebuild "$@";;
+    vm)        vm "$@";;
+    full)      full ;;
+    rollback)  rollback ;;
+    update)    update ;;
+    clean)     clean ;;
+    search)    search "$@" ;;
+    help)    help ;;
+    *)       help ;;
+esac
 
 ''

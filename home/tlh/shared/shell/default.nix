263ffeb605fc049a309538594ceba58e61eca08e
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2023-05-27 03:15:35 -0700

    rearrangement

--- home/tlh/shared/shell/default.nix
+++ home/tlh/shared/shell/default.nix
@@ -3,7 +3,15 @@
   lib,
   config,
   ...
-}: {
+}: let
+  extract = import ./bin/extract {inherit pkgs;};
+  gita = import ./bin/gita {inherit pkgs;};
+  nux = import ./bin/nux {inherit pkgs;};
+  preview = import ./bin/preview {inherit pkgs;};
+  run = import ./bin/run {inherit pkgs;};
+  updoot = import ./bin/updoot {inherit pkgs;};
+  vfio = import ./bin/vfio {inherit pkgs;};
+in {
   imports = [
     ./xdg.nix
     ./zsh.nix
@@ -24,7 +32,7 @@
       GOPATH = "${config.home.homeDirectory}/Workspace/go";
       GOBIN = "${config.home.homeDirectory}/Workspace/go/bin";
     };
-
+    home.packages = with pkgs; [extract gita nux preview run updoot vfio];
     #file = { };
   };
 }

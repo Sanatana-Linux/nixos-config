46124e194111cdcd27458b431b379940d2ded112
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2023-12-15 02:07:31 -0800

    remove floorp trash

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,7 @@
-{pkgs ? import <nixpkgs> {}}: {
-  firefox-gnome-theme = pkgs.callPackage ./firefox-gnome-theme {};
-  firefox-alpha-theme = pkgs.callPackage ./firefox-alpha-theme {};
-  macos-cursors = pkgs.callPackage ./macos-cursors {};
-  material-symbols = pkgs.callPackage ./material-symbols {};
-  lightdm-webkit2-greeter = pkgs.callPackage ./lightdm-webkit2-greeter {};
+{ pkgs ? import <nixpkgs> { } }: {
+  firefox-gnome-theme = pkgs.callPackage ./firefox-gnome-theme { };
+  higgs-boson = pkgs.callPackage ./higgs-boson { };
+  macos-cursors = pkgs.callPackage ./macos-cursors { };
+  material-symbols = pkgs.callPackage ./material-symbols { };
+  lightdm-webkit2-greeter = pkgs.callPackage ./lightdm-webkit2-greeter { };
 }

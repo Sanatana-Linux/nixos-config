352aeb105f384f03bb20ad4fd73d4a45c6973dd2
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2024-03-20 03:59:17 -0700

    branched

--- pkgs/phocus/default.nix
+++ pkgs/phocus/default.nix
@@ -5,13 +5,13 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "phocus";
-  version = "0cf0eb35a927bffcb797db8a074ce240823d92de";
+  version = "";
 
   src = fetchFromGitHub {
     owner = "phocus";
     repo = "gtk";
     rev = version;
-    sha256 = "sha256-URuoDJVRQ05S+u7mkz1EN5HWquhTC4OqY8MqAbl0crk=";
+    sha256 = "";
   };
 
   patches = [

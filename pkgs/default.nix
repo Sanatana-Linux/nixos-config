e4c40b194f48149d328ab833c5682b0239503e3d
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2024-03-23 14:16:23 -0700

    ugh

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> { } }: {
   firefox-gnome-theme = pkgs.callPackage ./firefox-gnome-theme { };
-  phocus = pkgs.callPackage ./phocus { };
-  higgs-boson = pkgs.callPackage ./higgs-boson { };
+#  phocus = pkgs.callPackage ./phocus { };
+#@  higgs-boson = pkgs.callPackage ./higgs-boson { };
   macos-cursors = pkgs.callPackage ./macos-cursors { };
   material-symbols = pkgs.callPackage ./material-symbols { };
 }

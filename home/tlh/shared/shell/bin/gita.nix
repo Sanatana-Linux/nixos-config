ca6e82661e7d2c67b264f1e505985304d1bf859a
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2024-09-21 13:35:01 -0700

    gita

--- home/tlh/shared/shell/bin/gita.nix
+++ home/tlh/shared/shell/bin/gita.nix
@@ -3,10 +3,10 @@ with pkgs;
   writeScriptBin "gita" ''
     #!/usr/bin/env bash
 
-    git add --all .
+    git add .
 
-    git commit $1 || git commit
+    git commit -m "$(date +"%Y-%m-%d %H:%M:%S")"
 
-    git push \
+    git push
 
   ''

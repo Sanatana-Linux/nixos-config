5fc00cd33fcc6166632868f60687db6be5b35f69
Thomas Leon Highbaugh <me@thomasleonhighbaugh.me>
2025-02-26 18:33:34 -0800

    nvidia comments

--- hosts/shared/hardware/nvidia.nix
+++ hosts/shared/hardware/nvidia.nix
@@ -124,9 +124,19 @@ in {
       #  forceFullCompositionPipeline = true;
       dynamicBoost.enable = true;
       powerManagement = {
+        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
         enable = false;
+        # Fine-grained power management. Turns off GPU when not in use.
+        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
         finegrained = false;
       };
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      # Currently alpha-quality/buggy, so false is currently the recommended setting.
       open = false;
       package = nvidiaDriverChannel;
       prime = {

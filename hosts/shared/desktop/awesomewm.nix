4792eda3b3d45a5f5d332db1741d93ddc7bdb3b8
Thomas Leon Highbaugh <me@thomasleonhighbaugh.me>
2025-11-26 21:53:07 -0800

    awesome

--- hosts/shared/desktop/awesomewm.nix
+++ hosts/shared/desktop/awesomewm.nix
@@ -13,54 +13,31 @@
     xserver = {
       windowManager.awesome = {
         enable = true;
-
-        package = inputs.nixpkgs-f2k.packages.${pkgs.system}.awesome-luajit-git.overrideAttrs (prevAttrs: {
-          GI_TYPELIB_PATH = let
-            mkTypeLibPath = pkg: "${pkg}/lib/girepository-1.0";
-            extraGITypeLibPaths =
-              lib.forEach (
-                with pkgs; [
-                  networkmanager
-                  upower
-                  cairo
-                  goocanvas
-                  librsvg
-                  gobject-introspection-unwrapped
-                ]
-              )
-              mkTypeLibPath;
-          in
-            lib.concatStringsSep ":" (extraGITypeLibPaths ++ [(mkTypeLibPath pkgs.pango.out)]);
-        });
-        # List of Lua modules to be available for AwesomeWM.
-        luaModules = with pkgs.luajitPackages;
-          [
-            luautf8 # text handling
-            luaposix # POSIX bindings for Lua.
-            cqueues # Networking, event loop, and coroutine library.
-            cjson # Fast JSON parsing and encoding support.
-            dkjson # Pure Lua JSON module.
-            ldbus # D-Bus bindings for Lua.
-            ldoc # Documentation generator for Lua code.
-            lgi # GObject Introspection for Lua.
-            lpeg # Parsing Expression Grammars for Lua.
-            lpeg_patterns # Common patterns for LPeg.
-            lpeglabel # LPeg extension for labeled failures.
-            lua # The Lua language itself.
-            lua-messagepack # MessagePack serialization for Lua.
-            luarocks # Package manager for Lua modules.
-            luasocket # Networking support for Lua.
-            luasql-sqlite3 # SQLite3 bindings for LuaSQL.
-            mpack # MessagePack implementation for Lua.
-            std-_debug # Debugging utilities for Lua.
-            std-normalize # Normalization utilities for Lua.
-            stdlib # Standard library for Lua.
-            vicious # Widgets for AwesomeWM.
-            wrapLua # Utility for wrapping Lua scripts.
-          ]
-          ++ [
-            pkgs.lua51Packages.lgi # Lua 5.1 LGI for compatibility
-          ];
+        package = (import ../../../pkgs {inherit pkgs;}).awesome-luajit;
+        luaModules = with pkgs.luajitPackages; [
+          luautf8
+          luaposix
+          cqueues
+          cjson
+          dkjson
+          ldbus
+          ldoc
+          lgi
+          lpeg
+          lpeg_patterns
+          lpeglabel
+          lua
+          lua-messagepack
+          luarocks
+          luasocket
+          luasql-sqlite3
+          mpack
+          std-_debug
+          std-normalize
+          stdlib
+          vicious
+          wrapLua
+        ] ++ [pkgs.lua51Packages.lgi];
       };
     }; # ends xserver
   }; # ends services
@@ -68,31 +45,32 @@
   # ------------------------------------------------- #
   # System packages required for AwesomeWM and its ecosystem.
   environment.systemPackages = with pkgs; [
-    i3lock-fancy-rapid # Lock screen utility.
-    luabind_luajit # Lua bindings for C++ using LuaJIT.
-    lua51Packages.lua # Lua 5.1 interpreter.
-    lua51Packages.lgi # Lua 5.1 packages for luajit compatibility.
-    gsettings-desktop-schemas # Schemas for GSettings.
-    gobject-introspection-unwrapped # GObject introspection support.
-    eggdbus # D-Bus utilities for GObject.
-    scrot # Screenshot utility.
-    maim # Screenshot utility.
-    satty # Screenshot annotation tool.
-    menu-cache # Caching mechanism for freedesktop.org menus.
-    xfce.garcon # Freedesktop.org menu library.
-    xfce.libxfce4ui # XFCE UI library.
-    xfce.libxfce4util # XFCE utility library.
-    xfce.tumbler # Thumbnail service for XFCE.
-    gdk-pixbuf # Image loading library.
-    gdk-pixbuf-xlib # Xlib integration for gdk-pixbuf.
-    xdotool # Simulate keyboard/mouse input.
-    inputs.lemonake.packages.${pkgs.system}.lua-pam-git # PAM bindings for Lua.
-    xsel # Manipulate X selections.
-    xsettingsd # XSETTINGS daemon.
-    dconf-editor # Editor for dconf settings.
-    xorg.xwininfo # Window information utility.
-    xdg-launch # XDG launcher utility.
-    xdg-utils # XDG utilities.
+    glib
+    i3lock-fancy-rapid
+    luabind_luajit
+    lua51Packages.lua
+    lua51Packages.lgi
+    gsettings-desktop-schemas
+    gobject-introspection-unwrapped
+    eggdbus
+    scrot
+    maim
+    satty
+    menu-cache
+    xfce.garcon
+    xfce.libxfce4ui
+    xfce.libxfce4util
+    xfce.tumbler
+    gdk-pixbuf
+    gdk-pixbuf-xlib
+    xdotool
+    inputs.lemonake.packages.${pkgs.system}.lua-pam-git
+    xsel
+    xsettingsd
+    dconf-editor
+    xorg.xwininfo
+    xdg-launch
+    xdg-utils
   ];
 
   security.pam.services.i3lock.enable = true;
@@ -106,7 +84,9 @@
       LUA_CPATH = "${pkgs.luajitPackages.luarocks}/lib/lua/${pkgs.luajit.luaversion}/?.so;${pkgs.lua51Packages.lgi}/lib/lua/5.1/?.so";
       PATH = ["${pkgs.luajitPackages.luarocks}/bin"];
       GI_TYPELIB_PATH = lib.concatStringsSep ":" [
+        "${pkgs.glib.dev}/lib/girepository-1.0"
         "${pkgs.gobject-introspection-unwrapped}/lib/girepository-1.0"
+        "${pkgs.gobject-introspection}/lib/girepository-1.0"
         "${pkgs.networkmanager}/lib/girepository-1.0"
         "${pkgs.upower}/lib/girepository-1.0"
         "${pkgs.cairo}/lib/girepository-1.0"
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,9 @@
-pkgs: {
+pkgs: 
+{
   material-symbols = pkgs.callPackage ./material-symbols {};
   sea-greeter = pkgs.callPackage ./sea-greeter.nix {};
+  awesome-luajit = import ./awesome-luajit.nix {
+    inherit pkgs;
+    inherit (pkgs) stdenv lib fetchFromGitHub;
+  };
 }

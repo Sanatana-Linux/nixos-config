d7dc11e08aa1ed57e8330bca596c7c6139d08bea
Thomas Leon Highbaugh <me@thomasleonhighbaugh.me>
2026-03-09 13:21:33 -0400

    2026-03-09 13:21:33

--- shell.nix
+++ shell.nix
@@ -10,10 +10,18 @@
       alejandra
       neovim
       silver-searcher
+      nodejs
+      pnpm
     ];
 
     # Enable experimental features without having to specify the argument
     NIX_CONFIG = "experimental-features = nix-command flakes";
-    # TODO Add shell hook and MOTD message
+    
+    shellHook = ''
+      SAY=$(echo -e "Sanatana \n Linux")
+      if command -v pnpx >/dev/null; then
+         pnpx cfonts "$SAY" --colors "#4FB0BE","#F25F89" --align center --font slick
+      fi
+    '';
   };
 }

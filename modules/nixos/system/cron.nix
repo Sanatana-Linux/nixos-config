e8dfe2e21238f7b0ed2621574d1bcdd9a3f0442f
Thomas Leon Highbaugh <me@thomasleonhighbaugh.me>
2026-05-04 13:34:48 -0400

    2026-05-04 13:34:48

---
+++ modules/nixos/system/cron.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, ... }:
+with lib; {
+  options.modules.system.cron = {
+    enable = mkEnableOption "cron daemon";
+
+    jobs = mkOption {
+      type = types.listOf (types.submodule {
+        options = {
+          command = mkOption {
+            type = types.str;
+            description = "Command to run";
+          };
+          schedule = mkOption {
+            type = types.str;
+            default = "0 6 * * 1";
+            description = "Cron schedule expression (default: weekly Monday 6am)";
+          };
+          user = mkOption {
+            type = types.str;
+            default = "root";
+            description = "User to run the job as";
+          };
+        };
+      });
+      default = [ ];
+      description = "Cron jobs managed by system cron";
+    };
+  };
+
+  config = mkIf config.modules.system.cron.enable {
+    services.cron = {
+      enable = true;
+      systemCronJobs = map (job: "${job.schedule} ${job.user} ${job.command}") config.modules.system.cron.jobs;
+    };
+  };
+}
